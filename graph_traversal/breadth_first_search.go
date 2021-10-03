65901d076ec97fefe97c5c20551022e7f84fb1a4
Rafael Jesus <rafaelljesus86@gmail.com>
2021-10-03 23:06:06 +0200

    tiny refactoring bfs

--- graph_traversal/breadth_first_search.go
+++ graph_traversal/breadth_first_search.go
@@ -1,22 +1,18 @@
 package graph_traversal
 
 func BFSTraverse(v *Vertex) []string {
-	visited := make(visitedMap)
+	visited := make(map[string]struct{})
 	visited[v.Value] = struct{}{}
 	arr := make([]string, 0)
 	arr = append(arr, v.Value)
-	queue := make([]*Vertex, 0)
-	queue = append(queue, v)
+	queue := v.AdjacentVertices
 	for len(queue) > 0 {
 		vertex := queue[0]
 		queue = queue[1:]
-		for _, av := range vertex.AdjacentVertices {
-			if _, ok := visited[av.Value]; ok {
-				continue
-			}
-			visited[av.Value] = struct{}{}
-			queue = append(queue, av)
-			arr = append(arr, av.Value)
+		if _, ok := visited[vertex.Value]; !ok {
+			queue = append(queue, vertex.AdjacentVertices...)
+			arr = append(arr, vertex.Value)
+			visited[vertex.Value] = struct{}{}
 		}
 	}
 	return arr
